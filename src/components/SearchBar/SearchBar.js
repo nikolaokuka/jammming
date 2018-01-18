84e573fa21fb6ace7f224eed6a7c7c1aadcd098d
Nikola Okuka <34208327+nikolaokuka@users.noreply.github.com>
2018-01-18 09:53:16 +0100

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,36 +2,36 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {term: ''};
-		this.handleClick = this.handleClick.bind(this);
-		this.handleTermChange = this.handleTermChange.bind(this);
-		this.handleOnKeyUp = this.handleOnKeyUp.bind(this);
-	}
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
+    this.handleClick = this.handleClick.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleOnKeyUp = this.handleOnKeyUp.bind(this);
+  }
 
-	handleTermChange(e){
-		this.setState({term: e.target.value});
-	}
+  handleTermChange(e){
+    this.setState({term: e.target.value});
+  }
 
-	handleClick(){
-		this.props.search(this.state.term);
-	}
+  handleClick(){
+    this.props.search(this.state.term);
+  }
 
-	handleOnKeyUp(e){
-		if(e.keyCode === 13){
-			this.props.search(this.state.term);
-		}
-	}
+  handleOnKeyUp(e){
+    if(e.keyCode === 13){
+      this.props.search(this.state.term);
+    }
+  }
 
-	render(){
-		return (
-			<div className="SearchBar">
-		    <input placeholder="Enter A Song, Album or Artist" onChange={this.handleTermChange} onKeyUp={this.handleOnKeyUp} />
-		    <a onClick={this.handleClick}>SEARCH</a>
-		  </div>
-		);
-	}
+  render(){
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album or Artist" onChange={this.handleTermChange} onKeyUp={this.handleOnKeyUp} />
+        <a onClick={this.handleClick}>SEARCH</a>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
